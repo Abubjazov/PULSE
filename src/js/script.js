aeb548f8a91216c1ce882b4f3abd4ab7e842a349
Radik Abubjazov <abubjazov@mail.ru>
2021-06-30 21:14:01 +0300

    свёрстана секция carousel

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 500,
+//         slidesToShow: 1,
+//         prevArrow: '<button type="button" class="slick-prev"><img src= "icons/chevron_left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src= "icons/chevron_right.png"></button>'
+//     });
+// });
+  const slider = tns({
+      container: '.carousel__inner',
+      items: 1,
+      slideBy: 'page',
+      autoplay: false,
+      controls: false,
+      nav: false
+    });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
