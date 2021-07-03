241d1748acee136822b827beba26efcaf26ddbc3
Radik Abubjazov <abubjazov@mail.ru>
2021-07-04 00:25:23 +0300

    произведена оптимизация сайта

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,6 @@
 
 $(document).ready(function() {
 
-  // $(document).ready(function(){
-  //   $('.carousel__inner').slick({
-  //     speed: 500,
-  //     slidesToShow: 1,
-  //     prevArrow: '<button type="button" class="slick-prev"><img src= "icons/chevron_left.png"></button>',
-  //     nextArrow: '<button type="button" class="slick-next"><img src= "icons/chevron_right.png"></button>'
-  // });
-
   const slider = tns({
     container: '.carousel__inner',
     items: 1,
